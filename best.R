5886698a4516b6d1ea7566978363c4bc6e5394fc
throsturg <throsturg@gmail.com>
2015-06-25 23:52:41 +0000

    small changes

--- best.R
+++ best.R
@@ -1,9 +1,9 @@
 best <- function(state, outcome) {
   ## Read outcome data
-  data <- read.csv("outcome-of-care-measures.csv", colClass="character")
+  data <- read.csv("outcome-of-care-measures.csv", na.string="Not Available", stringsAsFactors=FALSE)
   outcome.lookup <- c("heart attack"=11,"heart failure"=17, "pneumonia"=23)
   outcome.index <- outcome.lookup[outcome]
-  ## data[,11] <- as.numeric(data[,outcome.index])
+  ## data[,outcome.index] <- as.numeric(data[,outcome.index])
   
   ## Check that state and outcome are valid
   validState <- state %in% data[,7]
@@ -17,6 +17,7 @@ best <- function(state, outcome) {
   }
   
   ## Return hospital name in that state with lowest 30-day death rate
-  sapply(data[data$State==state,c(2, outcome.index)], min, na.rm=TRUE)[[1]]
-
+  data.state <- data[data$State==state,c(2,outcome.index)]
+  data.state[which.min(data.state[[2]]),][[1]]
+  
 }
